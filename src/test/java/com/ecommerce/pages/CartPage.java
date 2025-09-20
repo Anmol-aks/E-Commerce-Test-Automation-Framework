32eda751b230707e05cc68521aa56afefdef923d
Anmol-aks <anmolsingh14704@gmail.com>
2025-09-20 18:57:13 +0530

    fix: Use JavaScript click on checkout button for stability

--- src/test/java/com/ecommerce/pages/CartPage.java
+++ src/test/java/com/ecommerce/pages/CartPage.java
@@ -1,5 +1,6 @@
 package com.ecommerce.pages;
 
+import org.openqa.selenium.JavascriptExecutor; // <-- IMPORT THIS
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,7 +19,7 @@ public class CartPage {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
-        // THE FIX: Wait for the button to be clickable, not just visible.
+        // This wait ensures the button is ready before any action is taken
         wait.until(ExpectedConditions.elementToBeClickable(checkoutButton));
     }
 
@@ -26,6 +27,8 @@ public class CartPage {
     private WebElement checkoutButton;
 
     public void proceedToCheckout() {
-        checkoutButton.click();
+        // THIS IS THE FIX: Use a JavascriptExecutor for a more reliable click
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", checkoutButton);
     }
 }
