7dba1f3fa8fcea09058f2adf8fad57c352aa0c51
Anmol-aks <anmolsingh14704@gmail.com>
2025-09-20 17:35:41 +0530

    fix: Add explicit wait for cart badge to appear and cart page to load

--- src/test/java/com/ecommerce/pages/CartPage.java
+++ src/test/java/com/ecommerce/pages/CartPage.java
@@ -1,18 +1,25 @@
-
 package com.ecommerce.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CartPage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
+        // Wait for the checkout button to be visible when the page object is created
+        wait.until(ExpectedConditions.visibilityOf(checkoutButton));
     }
 
     @FindBy(id = "checkout")
--- src/test/java/com/ecommerce/pages/ProductsPage.java
+++ src/test/java/com/ecommerce/pages/ProductsPage.java
@@ -4,16 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ProductsPage {
 
     WebDriver driver;
+    WebDriverWait wait; // <-- DECLARE A WAIT OBJECT
 
-    public ProductsPage(WebDriver driver){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
+    // Constructor
+    public ProductsPage(WebDriver driver) {
+        this.driver = driver;
+        // Initialize the WebDriverWait for a 10-second timeout
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // INITIALIZE THE WAIT
+        PageFactory.initElements(driver, this);
     }
 
+    // Locators
     @FindBy(id = "add-to-cart-sauce-labs-backpack")
     private WebElement backpackAddToCartButton;
 
@@ -23,16 +32,19 @@ public class ProductsPage {
     @FindBy(className = "shopping_cart_link")
     private WebElement shoppingCartLink;
 
+    // Action Methods
     public void addBackpackToCart() {
         backpackAddToCartButton.click();
+        //Wait until the shopping cart badge is visible on the page
+        wait.until(ExpectedConditions.visibilityOf(shoppingCartBadge));
     }
 
     public String getCartItemCount() {
+        // the badge is present
         return shoppingCartBadge.getText();
     }
 
     public void goToCart() {
         shoppingCartLink.click();
     }
-
 }
