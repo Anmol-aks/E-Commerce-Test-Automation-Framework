60ed5742a2f9837a852a8f2f1e4a94572afa3a4b
Anmol-aks <anmolsingh14704@gmail.com>
2025-09-20 18:53:23 +0530

    fix: Add explicit wait for checkout info page to load

--- src/test/java/com/ecommerce/pages/CheckoutInfoPage.java
+++ src/test/java/com/ecommerce/pages/CheckoutInfoPage.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions; // <-- IMPORT THIS
+import org.openqa.selenium.support.ui.WebDriverWait;         // <-- IMPORT THIS
+
+import java.time.Duration; // <-- IMPORT THIS
 
 public class CheckoutInfoPage {
 
     WebDriver driver;
+    WebDriverWait wait; // <-- DECLARE A WAIT OBJECT
 
     public CheckoutInfoPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // <-- INITIALIZE THE WAIT
         PageFactory.initElements(driver, this);
+        // THIS IS THE FIX: Wait for the first name field to be visible
+        wait.until(ExpectedConditions.visibilityOf(firstNameField));
     }
 
     @FindBy(id = "first-name")
